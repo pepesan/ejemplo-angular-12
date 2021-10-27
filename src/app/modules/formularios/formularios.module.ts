635756ebd4a037e67eaf9608b4c25237c071f583
David Vaquero <pepesan@gmail.com>
2021-10-27 13:38:58 +0200

    ejemplo de radios buttons generado sen base a un array de opciones

--- src/app/modules/formularios/formularios.module.ts
+++ src/app/modules/formularios/formularios.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MainFormComponent} from "./components/main-form/main-form.component";
 import {FormsModule} from "@angular/forms";
+import { RadiosComponent } from './components/radios/radios.component';
 
 @NgModule({
-  declarations: [MainFormComponent],
+  declarations: [MainFormComponent, RadiosComponent],
   exports: [
-    MainFormComponent
+    MainFormComponent,
+    RadiosComponent
   ],
   imports: [
     CommonModule,
