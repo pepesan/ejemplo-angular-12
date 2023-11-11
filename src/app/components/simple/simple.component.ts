9883ad30b2335e3df8e34850ddd5cb3fc4ca41ce
David Vaquero <pepesan@gmail.com>
2023-11-11 18:45:41 +0100

    los nuevos componentes

---
+++ src/app/components/simple/simple.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple',
+  templateUrl: './simple.component.html',
+  styleUrl: './simple.component.sass'
+})
+export class SimpleComponent {
+
+}
